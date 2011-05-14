3469ab2cd5dcaa9c7d452517fc02ac85fc8f746c
mathphreak <mathphreak@gmail.com>
2011-05-13 20:58:37 -0600

    tested huge images and added support for horizontal scroll, too

--- pecan.js
+++ pecan.js
@@ -3,13 +3,14 @@ if (!!amplify.store && !!jQuery) {
 	window.onscroll = function() {
 		if (!document.body) return; // if the DOM isn't quite ready...then don't touch it.
 		var currPos = document.body.scrollTop;
-		amplify.store("pecan_scroll_position", currPos);
+		amplify.store("pecan_vpos", currPos);
+		currPos = document.body.scrollLeft;
+		amplify.store("pecan_hpos", currPos);
 	}
 	jQuery(function() {
-		var storedPos = amplify.store("pecan_scroll_position");
-		if (!!storedPos) {
-			window.scrollTo(0, storedPos);
-		}
+		var vPos = amplify.store("pecan_vpos");
+		var hPos = amplify.store("pecan_hpos");
+		window.scrollTo(!hPos ? 0 : hPos, !vPos ? 0 : vPos);
 	});
 } else {
 	document.write("Pecan.js can't find amplify.store, or it can't find jQuery.  Make sure both of those exist when Pecan.js loads.");
