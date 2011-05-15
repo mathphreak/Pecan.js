014739c99b31b4ab9cee319b4b22d99c9ca14b4a
mathphreak <mathphreak@gmail.com>
2011-05-15 14:24:41 -0600

    allowed for finer control of scrolling time

--- pecan.js
+++ pecan.js
@@ -1,25 +1,38 @@
 // If you don't have the amplify.js storage module and jQuery, then this won't work.  Period.
 if (!!amplify.store && !!jQuery) {
-	window.onscroll = function() {
+	jQuery(window).scroll(function() {
 		if (!document.body) return; // if the DOM isn't quite ready...then don't touch it.
 		var currPos = document.body.scrollTop;
 		amplify.store("pecan_vpos", currPos);
 		currPos = document.body.scrollLeft;
 		amplify.store("pecan_hpos", currPos);
-	}
-	// this is sort of complicated...
-	// do vertical scrolling (down the page) first
-	jQuery(function() {
+	});
+	var Pecan = {};
+	Pecan.init = function() {
+		window.scrollTo(0, 0);
+	};
+	Pecan.loadVert = function() {
 		var vPos = amplify.store("pecan_vpos");
 		var realV = !vPos ? 0 : vPos;
-		window.scrollTo(0, realV);
-	});
-	// and do horizontal scrolling (across the page) after all images load
-	jQuery(window).load(function() {
+		window.scrollBy(0, realV);
+	};
+	Pecan.loadHoriz = function() {
 		var hPos = amplify.store("pecan_hpos");
 		var realH = !hPos ? 0 : hPos;
 		window.scrollBy(realH, 0);
-	});
+	};
+	Pecan.load = function() {
+		Pecan.init();
+		Pecan.loadVert();
+		Pecan.loadHoriz();
+	};
+	Pecan.useDefaultEvents = function() {
+		jQuery(function() {
+			Pecan.init();
+			Pecan.loadVert();
+		});
+		jQuery(window).load(Pecan.loadHoriz);
+	};
 } else {
 	document.write("Pecan.js can't find amplify.store, or it can't find jQuery.  Make sure both of those exist when Pecan.js loads.");
 }
